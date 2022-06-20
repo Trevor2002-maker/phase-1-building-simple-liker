6fa442934703ff308f95906d25216a6529a8a001
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-20 08:02:21 +0300

    Done.

--- main.js
+++ main.js
@@ -4,9 +4,28 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
-
-
+function turnLike(like){
+  if(like.textContent === EMPTY_HEART){
+    like.textContent = FULL_HEART;
+    like.style.color="red";
+  }
+  else{
+    like.textContent = EMPTY_HEART
+  }
+}
+const likes = document.querySelectorAll('.like-glyph');
+const blankError = document.querySelector('#modl');
+const modalmsg = document.querySelector('#modl-msg');
+hiddenError.classList.add('hidden');
+likes.forEach((like)=>like.addEventListener('click',()=>{
+  mimicServerCall()
+  .then(()=> turnLike(like))
+  .catch((error)=>display(error));
+}));
+function display(errorMessage){
+  blankError.classList.remove('hidden')
+  modalmsg.textContent = errorMessage;
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
